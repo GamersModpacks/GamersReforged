7821fffbf35a37c0ae10a463b360f87d88b53680
Den4enko <dima.den4enko.ok@gmail.com>
2021-12-15 21:00:06 +0200

    1

--- build_pack.bat
+++ build_pack.bat
@@ -3,7 +3,7 @@ rmdir /s /q release
 mkdir release
 xcopy /y /e "snapshot\*.*" "release\*.*"
 del /q release\pack.toml
-copy pack.toml release\pack.toml
+copy pack_info\pack.toml release\pack.toml
 cd release
 packwiz refresh
 timeout /t 3 /nobreak
