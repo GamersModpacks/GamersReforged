a6ec58a725d74fda8a0f105739430a15ef2de171
Den4enko <dima.den4enko.ok@gmail.com>
2021-12-15 22:04:54 +0200

    1

--- build_pack.bat
+++ build_pack.bat
@@ -6,6 +6,7 @@ del /q release\pack.toml
 copy pack_info\pack.toml release\pack.toml
 del /q release\packmenu\resources\assets\packmenu\lang\en_us.json
 del /q release\packmenu\resources\assets\packmenu\buttons\reloadbutton.json
+del /q release\packmenu\resources\assets\packmenu\textures\gui\reload.png
 copy pack_info\packmenu_language.json release\packmenu\resources\assets\packmenu\lang\en_us.json
 cd release
 packwiz refresh
