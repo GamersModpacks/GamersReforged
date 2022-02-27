00efed25d385fb025c579d5bcc6a49e82d49c632
Дмитрий Деньченко <60628208+Den4enko@users.noreply.github.com>
2022-02-27 15:03:22 +0200

    Fix build script

--- build_pack.bat
+++ build_pack.bat
@@ -3,10 +3,10 @@ rmdir /s /q release
 mkdir release
 xcopy /y /e "snapshot\*.*" "release\*.*"
 del /q release\pack.toml
-copy pack_info\pack.toml release\pack.toml
 del /q release\packmenu\resources\assets\packmenu\lang\en_us.json
 del /q release\packmenu\resources\assets\packmenu\buttons\reloadbutton.json
 del /q release\packmenu\resources\assets\packmenu\textures\gui\reload.png
+copy pack_info\pack.toml release\pack.toml
 copy pack_info\packmenu_language.json release\packmenu\resources\assets\packmenu\lang\en_us.json
 cd release
 packwiz refresh
